efd5e27efbe9428c2901f4fe24b9db998e684f80
matteo-grella <matteogrella@gmail.com>
2023-07-18 11:16:56 +0200

    Add size check in broadcasting functions

--- src/modules/mod_arr_ops_broadcasting.f90
+++ src/modules/mod_arr_ops_broadcasting.f90
@@ -1,8 +1,12 @@
 ! Copyright 2023 FortAI-Hub contributors.
 ! Released under the MIT License. See LICENSE file for full license information.
 
-! This module, mod_arr_ops_broadcasting, extends the basic algebraic operations (*/+-) with broadcasting.
+! This module, mod_arr_ops_broadcasting, extends the basic algebraic operations (*/+-) with column-wise broadcasting.
 ! Broadcasting is a technique that allows arithmetic operations between arrays of different shapes.
+! In column-wise broadcasting, the function checks whether the size of the 1D array matches the number of rows in the 2D array.
+! If the sizes match, the function applies the broadcasting, treating the 1D array as a column vector and multiplying it with each column of the 2D array.
+!
+! Note that row-wise broadcating is currently not supported.
 module mod_arr_ops_broadcasting
     use mod_real_precision
     implicit none
@@ -37,7 +41,14 @@ contains
         real(sp), intent(in) :: b(:,:)
         real(sp) :: c(size(b, 1), size(b, 2))
 
+        character(len=100) :: errmsg
         integer :: i, n
+
+        if (size(b, 1) /= size(a)) then
+            write(errmsg, '(A, I0, A, I0, A)') "Error: The size of 'a' (", size(a), ") does not match the number of rows in 'b' (", size(b, 1), ")."
+            error stop trim(errmsg)
+        end if
+
         n = size(b, 2)
 
         do concurrent (i=1:n)
@@ -50,7 +61,14 @@ contains
         real(sp), intent(in) :: b(:)
         real(sp) :: c(size(a, 1), size(a, 2))
 
+        character(len=100) :: errmsg
         integer :: i, n
+
+        if (size(a, 1) /= size(b)) then
+            write(errmsg, '(A, I0, A, I0, A)') "Error: The size of 'b' (", size(b), ") does not match the number of rows in 'a' (", size(a, 1), ")."
+            error stop trim(errmsg)
+        end if
+
         n = size(a, 2)
 
         do concurrent (i=1:n)
@@ -63,7 +81,14 @@ contains
         real(sp), intent(in) :: b(:,:)
         real(sp) :: c(size(b, 1), size(b, 2))
 
+        character(len=100) :: errmsg
         integer :: i, n
+
+        if (size(b, 1) /= size(a)) then
+            write(errmsg, '(A, I0, A, I0, A)') "Error: The size of 'a' (", size(a), ") does not match the number of rows in 'b' (", size(b, 1), ")."
+            error stop trim(errmsg)
+        end if
+
         n = size(b, 2)
 
         do concurrent (i=1:n)
@@ -77,6 +102,13 @@ contains
         real(sp) :: c(size(a, 1), size(a, 2))
 
         integer :: i, n
+        character(len=100) :: errmsg
+
+        if (size(a, 1) /= size(b)) then
+            write(errmsg, '(A, I0, A, I0, A)') "Error: The size of 'b' (", size(b), ") does not match the number of rows in 'a' (", size(a, 1), ")."
+            error stop trim(errmsg)
+        end if
+
         n = size(a, 2)
 
         do concurrent (i=1:n)
@@ -89,7 +121,14 @@ contains
         real(sp), intent(in) :: b(:,:)
         real(sp) :: c(size(b, 1), size(b, 2))
 
+        character(len=100) :: errmsg
         integer :: i, n
+
+        if (size(b, 1) /= size(a)) then
+            write(errmsg, '(A, I0, A, I0, A)') "Error: The size of 'a' (", size(a), ") does not match the number of rows in 'b' (", size(b, 1), ")."
+            error stop trim(errmsg)
+        end if
+
         n = size(b, 2)
 
         do concurrent (i=1:n)
@@ -103,6 +142,13 @@ contains
         real(sp) :: c(size(a, 1), size(a, 2))
 
         integer :: i, n
+        character(len=100) :: errmsg
+
+        if (size(a, 1) /= size(b)) then
+            write(errmsg, '(A, I0, A, I0, A)') "Error: The size of 'b' (", size(b), ") does not match the number of rows in 'a' (", size(a, 1), ")."
+            error stop trim(errmsg)
+        end if
+
         n = size(a, 2)
 
         do concurrent (i=1:n)
@@ -115,7 +161,14 @@ contains
         real(sp), intent(in) :: b(:,:)
         real(sp) :: c(size(b, 1), size(b, 2))
 
+        character(len=100) :: errmsg
         integer :: i, n
+
+        if (size(b, 1) /= size(a)) then
+            write(errmsg, '(A, I0, A, I0, A)') "Error: The size of 'a' (", size(a), ") does not match the number of rows in 'b' (", size(b, 1), ")."
+            error stop trim(errmsg)
+        end if
+
         n = size(b, 2)
 
         do concurrent (i=1:n)
@@ -129,6 +182,13 @@ contains
         real(sp) :: c(size(a, 1), size(a, 2))
 
         integer :: i, n
+        character(len=100) :: errmsg
+
+        if (size(a, 1) /= size(b)) then
+            write(errmsg, '(A, I0, A, I0, A)') "Error: The size of 'b' (", size(b), ") does not match the number of rows in 'a' (", size(a, 1), ")."
+            error stop trim(errmsg)
+        end if
+
         n = size(a, 2)
 
         do concurrent (i=1:n)
